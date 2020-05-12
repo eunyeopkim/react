49912c53cf9256ccc32026b1abf3bb4281c32c77
Eunyeop Kim <dmsduqzz@gmail.com>
2020-05-12 10:58:44 +0900

    kanban-app 깔끔 정리

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import KanbanBoard from './KanbanBoard';
 import data from './data.json';
+
+import './App.css'
+
 export default class App extends React.Component{
     render(){
         return(
-            <div className='App'>
+            <div className="App">
                 <KanbanBoard cards= { data } />
             </div>
         )
